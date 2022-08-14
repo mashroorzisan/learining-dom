e49f1f8c86c097810d5201382fd1afcb2208ea49
mashroorzisan <mashroorzisan@gmail.com>
2022-08-14 18:33:14 +0600

    added append

---
+++ append.js
@@ -0,0 +1,41 @@
+//1.where to be added
+const listplace = document.getElementsByClassName('list');
+
+//2.what to be added
+const li = document.createElement('li');
+li.innerText = 'greece';
+
+//3.add the child
+listplace[0].appendChild(li);
+
+//1.where to be added
+const mainContainer = document.getElementById('main-cont');
+
+//2.what to b added
+const sectionLast = document.createElement('section');
+const h1 = document.createElement('h1');
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
+const li4 = document.createElement('li');
+const li5 = document.createElement('li');
+
+h1.innerText = "The companies I wanna work for";
+li1.innerText = "Meta";
+li2.innerText = "Amazon";
+li3.innerText = "Apple";
+li4.innerText = "Netflix";
+li5.innerText = "Google";
+
+//3.append the child
+sectionLast.appendChild(h1);
+sectionLast.appendChild(ul);
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+ul.appendChild(li4);
+ul.appendChild(li5);
+mainContainer.appendChild(sectionLast);
+
+//append sections and nodes directly
--- style-w-js.js
+++ style-w-js.js
@@ -1,12 +1,12 @@
-const sections = document.querySelectorAll('section');
-for (const section of sections) {
-    section.style.border = '2px solid black';
-    section.style.margin = '5px';
-    section.style.padding = '5px';
-    section.style.borderRadius = '5px';
-    section.style.backgroundColor = 'lightgrey';
-}
+// const sections = document.querySelectorAll('section');
+// for (const section of sections) {
+//     section.style.border = '2px solid black';
+//     section.style.margin = '5px';
+//     section.style.padding = '5px';
+//     section.style.borderRadius = '5px';
+//     section.style.backgroundColor = 'lightgrey';
+// }
+// // const sect = document.getElementById('two');
+// // sect.style.backgroundColor = 'red';
 // const sect = document.getElementById('two');
-// sect.style.backgroundColor = 'red';
-const sect = document.getElementById('two');
-sect.classList.add('yellow-bg'); 
+// sect.classList.add('yellow-bg'); 
